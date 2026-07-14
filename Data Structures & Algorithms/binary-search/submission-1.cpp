26d4346a3a6f5fde0aac83e8e8d3ffd581c3de48
xinyan.huang <huangxinyan20040826@gmail.com>
2026-07-14 11:55:02 +0800

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int right=nums.size()-1;
+        int left=0;
+        int mid=0;
+        //找中位數根要找的數字比大小，若比較大就讓最右側變成中位數-1，同理左側+1
+        while(right>=left){
+            mid=(right-left)/2+left;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[mid]>target){
+                right=mid-1;
+            }
+            if(nums[mid]<target){
+                left=mid+1;
+            }
+        }
+        return -1;
+    }
+};
