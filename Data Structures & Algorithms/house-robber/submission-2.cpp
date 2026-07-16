f1c73944ae383d49f04af2a263b01b63ed0024ea
xinyan.huang <huangxinyan20040826@gmail.com>
2026-07-16 10:07:13 +0800

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if(nums.empty()) return 0;
+        if(nums.size()==1) return nums[0];
+        int prev1=nums[0];
+        int prev2=0;
+        for(int i=1;i<nums.size();i++){
+            int current(max(prev1,prev2+nums[i]));
+            prev2=prev1;
+            prev1=current;
+        }
+        return prev1;
+    }
+};
